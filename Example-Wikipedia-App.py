6a0162352dcdc59dc033082c422b48cd9a6707c6
Robot95 <nexys741641@gmail.com>
2019-01-10 19:37:21 +0530

    Update Example-Wikipedia-App.py

--- Example-Wikipedia-App.py
+++ Example-Wikipedia-App.py
@@ -3,5 +3,5 @@ import wikipedia
 
 # Takes the input and prints the summary of the entered keyword
 while True:
-        input = raw_input("Q: ")
-        print wikipedia.summary(input)
+        input = raw_input("Q: ") 
+        print wikipedia.summary(input) # print the data as per entered keyword
